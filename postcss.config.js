1c2bd1e286b7b4a1f09b8ad230930376c0a78068
Julien Deswaef <julien@xuv.be>
2025-07-17 12:05:29 +0200

    Creates a minified version of the CSS file

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: [
     require("autoprefixer")({}),
-    require("stylelint")({}),
     require('postcss-import')({}),
     require('cssnano')({
       preset: 'default',
